45da86f7f6ceb01e5c9edc2c5d3c3092c4b1b593
Patrick42702 <patrick42702@yahoo.com>
2025-06-26 10:29:43 -0400

    Temp disable authentication
    Temporarily disable user authentication

--- src/main/java/dev/golf_app/config/SecurityConfig.java
+++ src/main/java/dev/golf_app/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -22,15 +21,17 @@ public class SecurityConfig {
     http
       .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/api/auth/**",
-          "/api/users/register",
-          "/v3/api-docs/**",
-          "/swagger-ui/**",
-          "/swagger-ui.html")
-        .permitAll()
-        .anyRequest().authenticated()
-      )
-      .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+          .anyRequest().permitAll()
+//        .requestMatchers("/api/auth/**",
+//          "/api/users/register",
+//          "/v3/api-docs/**",
+//          "/swagger-ui/**",
+//          "/swagger-ui.html")
+//        .permitAll()
+//        .anyRequest().authenticated()
+//      )
+//      .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class
+      );
 
     return http.build();
   }
