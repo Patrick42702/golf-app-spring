bdfe82c8311386bf3cd5509a77bffd4e233f7755
Justin Variara <jvariara@gmail.com>
2025-06-30 14:33:17 -0400

    frontend initial setup

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </SafeAreaView>
+  );
+}
