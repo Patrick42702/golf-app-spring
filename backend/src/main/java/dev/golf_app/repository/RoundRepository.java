5222fbf7cd7d66401756ec59911e850ccb713449
Patrick42702 <patrick42702@yahoo.com>
2025-06-30 13:28:53 -0400

    Updated Round Repository

--- backend/src/main/java/dev/golf_app/repository/RoundRepository.java
+++ backend/src/main/java/dev/golf_app/repository/RoundRepository.java
@@ -1,9 +1,14 @@
 package dev.golf_app.repository;
 
 import dev.golf_app.models.Round;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface RoundRepository extends CrudRepository<Round, Integer> {
+public interface RoundRepository extends JpaRepository<Round, Integer> {
+
+  public Optional<Round> findById(Integer id);
+
 }
