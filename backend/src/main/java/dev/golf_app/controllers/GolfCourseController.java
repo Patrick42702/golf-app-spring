310bfeb61b363b73524b5238e02c093668728024
Patrick42702 <patrick42702@yahoo.com>
2025-07-01 11:00:47 -0400

    Altered golfcourse endpoints
    Altered golfcourse endpoints to return response entity for finer grain
    control of responses

--- backend/src/main/java/dev/golf_app/controllers/GolfCourseController.java
+++ backend/src/main/java/dev/golf_app/controllers/GolfCourseController.java
@@ -5,6 +5,7 @@ import dev.golf_app.repository.GolfCourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,33 +19,39 @@ public class GolfCourseController {
   private GolfCourseRepository golfCourseRepository;
 
   @GetMapping
-  public Page<GolfCourseDTO> getAllCourses(
+  public ResponseEntity<Page<GolfCourseDTO>> getAllCourses(
     @RequestParam(defaultValue = "0") int page,
     @RequestParam(defaultValue = "20") int size) {
 
       // Enforce maximum page size
       size = Math.min(size, 50);
-      return golfCourseRepository.findAllAsDto(PageRequest.of(page, size));
+      Page<GolfCourseDTO> courses = golfCourseRepository.findAllAsDto(PageRequest.of(page, size));
+      return ResponseEntity.ok(courses);
     }
 
   @GetMapping("/closest-courses")
-  public Page<GolfCourseDTO> getClosestCourses(
+  public ResponseEntity<Page<GolfCourseDTO>> getClosestCourses(
     @RequestParam(defaultValue = "0") int page,
     @RequestParam(defaultValue = "5") int size,
     @RequestParam() double latitude,
     @RequestParam() double longitude) {
 
     size = Math.min(size, 5);
-    return golfCourseRepository.findNearestCourses(latitude, longitude, PageRequest.of(page, size));
+    Page<GolfCourseDTO> courses = golfCourseRepository.findNearestCourses(
+      latitude,
+      longitude,
+      PageRequest.of(page, size));
+    return ResponseEntity.ok(courses);
   }
 
   @GetMapping("/state")
-  public Page<GolfCourseDTO> getStateAbbrCourses(
+  public ResponseEntity<Page<GolfCourseDTO>> getStateAbbrCourses(
     @RequestParam(defaultValue = "NY") String abbr,
     @RequestParam(defaultValue = "0") int page,
     @RequestParam(defaultValue = "10") int size){
 
-    return golfCourseRepository.findByStateAbbr(abbr, PageRequest.of(page, size));
+    Page<GolfCourseDTO> courses = golfCourseRepository.findByStateAbbr(abbr, PageRequest.of(page, size));
+    return ResponseEntity.ok(courses);
   }
 
 }
